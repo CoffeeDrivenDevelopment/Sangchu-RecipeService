76b6cf62f4af73bc886b09497990574cf4695863
KIMSEI1124 <workju1124@gmail.com>
2024-04-07 10:15:26 +0900

    Feat: Application 수정(#15)
    EnableFeignClients 추가

--- src/main/java/com/cdd/recipeservice/RecipeServiceApplication.java
+++ src/main/java/com/cdd/recipeservice/RecipeServiceApplication.java
@@ -1,16 +1,18 @@
 package com.cdd.recipeservice;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.boot.context.properties.*;
-import org.springframework.cache.annotation.*;
-import org.springframework.cloud.client.discovery.*;
-import org.springframework.data.jpa.repository.config.*;
-import org.springframework.scheduling.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ConfigurationPropertiesScan
 @EnableCaching
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
